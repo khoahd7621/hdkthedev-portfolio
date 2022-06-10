170a56db96c6f5498aece65c47e986a70a604985
Hoang Dang Khoa <hoangdangkhoa7621@gmail.com>
2022-06-10 17:21:32 +0700

    Add wrapper higher component (socials, nav, copyright)

--- src/container/About/About.jsx
+++ src/container/About/About.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { motion } from 'framer-motion';
 
+import { AppWrap } from '../../wrapper'
 import { images } from '../../constants'
 import './About.scss';
 import { urlFor, client } from '../../client'
@@ -41,4 +42,4 @@ const About = () => {
   )
 }
 
-export default About
+export default AppWrap(About, 'about')
---
+++ src/wrapper/AppWrapp.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { NavigationDots, SocialMedia } from "../components";
+
+const AppWrapp = (Component, idName, classNames) =>
+  function HOC() {
+    return (
+      <div id={idName} className={`app__container ${classNames}`}>
+        <SocialMedia />
+
+        <div className="app__wrapper app__flex">
+          <Component />
+
+          <div className="copyright">
+            <p className="p-text">&copy; 2022 Hoang Dang Khoa</p>
+            <p className="p-text">All rights reserved.</p>
+          </div>
+        </div>
+
+        <NavigationDots active={idName} />
+      </div>
+    );
+  };
+
+export default AppWrapp;
