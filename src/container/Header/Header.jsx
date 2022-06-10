7faa3c1a90f13f17c8d7b4a609439ff467ce30f5
Hoang Dang Khoa <hoangdangkhoa7621@gmail.com>
2022-06-10 23:29:25 +0700

    Update title

--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -29,7 +29,7 @@ const Header = () => {
             <span>👋</span>
             <div style={{ marginLeft: 20 }}>
               <p className="p-text">Hello, I am</p>
-              <h1 className="head-text">Hoang Dang Khoa</h1>
+              <h1 className="head-text">Dang Khoa</h1>
             </div>
           </div>
 
